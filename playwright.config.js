ece8a69c5a757a9cc3018825bd3452dd1985a734
AlexKomanov <akomanov88@gmail.com>
2025-07-07 05:14:31 +0300

    Adding the attachments loggers

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'on',
     slowMo: 500,
     
   },
